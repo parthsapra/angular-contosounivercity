0539dddaf118685fbe46149fd68958a0a8ec513c
parthsapra <parthsapra91@gmail.com>
2020-01-28 14:12:23 +0530

    1 file modified, 9 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { UserComponent } from './user/user.component';
+import { EditstudentComponent } from './user/editstudent/editstudent.component';
+import { CourseComponent } from './course/course.component';
+import { EditcourseComponent } from './course/editcourse/editcourse.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, UserComponent, EditstudentComponent, CourseComponent, EditcourseComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
